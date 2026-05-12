bbeeacbd205d19535343ca7f616e8e526883b0c0
3mm1myy <s2322767@edu.savonia.fi>
2026-05-12 18:45:45 +0300

    Initial commit

---
+++ src/main/java/com/example/service/StudentService.java
@@ -0,0 +1,33 @@
+package com.example.service;
+
+import com.example.entity.Student;
+import com.example.repository.StudentRepository;
+
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class StudentService {
+
+    private final StudentRepository studentRepository;
+
+    public StudentService(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
+    public List<Student> searchStudents(
+            String lastName,
+            String firstName,
+            String groupTag
+    ) {
+
+        return studentRepository.findAll(
+                StudentSpecification.filterStudents(
+                        lastName,
+                        firstName,
+                        groupTag
+                )
+        );
+    }
+}
